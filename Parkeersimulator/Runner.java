65d7c47e27fa8c4a3b1d6ec4892591d811e5369d
AntonKokSchool <anton3kok3school@gmail.com>
2017-01-20 16:22:33 +0100

    Test Classe Anton V2m

---
+++ Parkeersimulator/Runner.java
@@ -0,0 +1,11 @@
+package Parkeersimulator;
+
+/**
+ * Created by AntonKok on 20-1-2017.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        Simulator sim = new Simulator();
+        sim.run();
+    }
+}
